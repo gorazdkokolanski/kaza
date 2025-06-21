bb37f9cfd5ac0ee82bdf1cfd8f54df095bdbd4c1
gorazdkokolanski <130471310+gorazdkokolanski@users.noreply.github.com>
2025-06-21 10:17:19 +0200

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -363,7 +363,7 @@ const Home = () => {
           scrollHandler = () => {
             // how far past the “start” point (200px before top of container)
             const { top } = container.getBoundingClientRect();
-            let scrolledPast = top - 300;
+            let scrolledPast = top - 400;
 
             // only run if we've scrolled into that 200px window
             if (scrolledPast > 0) return;
